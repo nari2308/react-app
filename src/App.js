f09422828b314ceadee4fb09fbb41275cb178ce1
nari2308 <officialworkathome23@gmail.com>
2023-06-30 16:02:55 +0600

    Header commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './Components/Signup/Signup';
 import Login from './Components/Login/Login';
 import Home  from './Components/Home/Home';
+import Header from './Components/Header/Header';
 
 
 function App() {
   return (
     <>
+    <Header />
+    <Home />
     <Router>
     <Routes>
-      <Route exact path='/' Component={App}></Route>
-      <Route  path='/home' Component={Home }></Route>
-      <Route  path='/signup' Component={Signup }></Route>
-     <Route  path='/login' Component={Login }></Route>
+      <Route exact path='react-app/' Component={App}></Route>
+      <Route  path='react-app/home' Component={Home }></Route>
+      <Route  path='react-app/signup' Component={Signup }></Route>
+     <Route  path='react-app/login' Component={Login }></Route>
     </Routes>
    </Router>
    </>
